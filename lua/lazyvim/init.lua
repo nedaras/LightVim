5ea12bdf80dffb257dbe109a7324235e804834af
Nedas Pranskunas <nedas.pranskunas@gmail.com>
2025-11-30 01:49:20 +0200

    Mocks colorscheme.

---
+++ lua/lazyvim/init.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+M.opts = {}
+
+function M.setup(opts)
+  local LazyVim = require("lazy.util")
+
+	M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+
+  LazyVim.try(function()
+    if type(M.opts.colorscheme) == "function" then
+      M.opts.colorscheme()
+    else
+      vim.cmd.colorscheme(M.opts.colorscheme)
+    end
+  end, {
+    msg = "Could not load your colorscheme",
+    on_error = function(msg)
+      LazyVim.error(msg)
+      vim.cmd.colorscheme("habamax")
+    end,
+  })
+end
+
+return M
